e1ac5587aa7b5ff8b06b74b2baaa09d3ac96bab5
barondim <108793615+barondim@users.noreply.github.com>
2024-06-04 22:54:25 +0000

    Deploying to gh-pages from @ barondim/dynawo@6caa18b9d91042e404abec94af118149b9b68af1 🚀

---
+++ navtreedata.js
@@ -0,0 +1,96 @@
+/*
+@ @licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2017 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Dynawo", "index.html", [
+    [ "Todo List", "de/d91/a01034.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", "functions_eval" ],
+        [ "Related Functions", "functions_rela.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"d0/d78/a01745.html#a1955a14d395747a90016e1f4193f932d",
+"d0/d9d/a03289.html#a9de44061487a1d84b3b0a3dbb9c0c7f8",
+"d0/df8/a02717.html#a7b3bdafd4d8ac0234becf71aa1eefc5c",
+"d1/d94/a02505.html#a437103d96a05e5a1143108950e946e5d",
+"d1/df1/a03217.html#a90a6e3177f5748a32bffe02f466c450b",
+"d2/d76/a03021.html#ade0d9df4fb6d5674e0e291518de9ed8d",
+"d2/dfa/a03285.html#a5d80e0b4af021777ec5be2a7048feeb7",
+"d3/d85/a02813.html#a8089dfd2e85bdc3fc21030715c846872",
+"d4/d39/a02965.html#ae59825bbca0abc23592e53230de03ef8",
+"d4/ddf/a00488.html#aabeca097df4bc0136b0a6f7867bf3f4b",
+"d5/da4/a03225.html#ac9fd60e22d8f47f0fb9d7911f7a4836d",
+"d5/dcb/a03121.html#af57a4e9a052a2348cf9de66525946f90",
+"d6/d10/a02621.html#a963225344a251150ba0ad6d5264f567faaef61fa8c2430427f08f04dea6062d14",
+"d6/d8d/a01007.html#ae48eb449c5ebb36d96867a60dbda3a1dac9db5d3942e95291b0aba717f2e863a6",
+"d7/d10/a01785.html#ac4de5ada071a1a49deeebdcf42a71d1a",
+"d7/d5e/a00467.html#a395d53feecd9c318b7b409cdbdd7b4db",
+"d7/dee/a03205.html#a12aa48a3616c04d3bd3bfdf8e9e833a6a663b6a5f6612497e7de32eb27f3685a9",
+"d8/d5d/a03329.html#a124d6cfc35ae95fbc5b5b2068676d8e8",
+"d8/dde/a03313.html#a12faa39683e5bc908d83e6bfa544e397",
+"d9/d65/a01905.html#a746d2836b2fbc2a8a31569353e53ea69",
+"d9/da5/a02757.html#a8b4a440e4a001e0da6890058e853a960",
+"d9/dfe/a02649.html#a9294fd85b78754e1a11fec544395ad51a65ceb795544a09aadbff2e8e4e78ad29",
+"da/d03/a02577.html#acfce33066451c48351076e31bd5b4ecea807ceba7261417e5fb3855a671363cde",
+"da/d0a/a03297.html#add6f6caa3b592fd1943a1b1160e41336a7360ad2352dc5247c847bd0f35884c1e",
+"da/d34/a03237.html#a563d3d64cffb951e135c95b30a1eaf08",
+"da/dd2/a02701.html#a676368141d6d501f2bd514d8d1be0a64",
+"db/d6a/a02613.html",
+"db/de9/a02821.html#ae2ec54eb015533425cecb9c7ea3d69b9",
+"dc/d9a/a00218.html",
+"dc/df1/a03261.html#a34d61d417c21fa5db51a44962c1eba75",
+"dd/d07/a03233.html#a823678b0d8f075e1015fcdb3872c26cc",
+"dd/d75/a02377.html",
+"de/d76/a02417.html#a2f1ade26aca2c2b2a1847a61558f36e5",
+"df/d2e/a01737.html#a580ea3a05ce7ff978023aa47765c4032",
+"df/d62/a03257.html#a3c17731aa465182f05559e0469b6f776",
+"df/de4/a02241.html#a8a7fde8991f890aefd3c6c6117f86e32"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
